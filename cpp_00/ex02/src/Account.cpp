bc2742942dd30173212f789135b1303bdd1f4242
icunha-t <isabeltootill@gmail.com>
2025-08-08 14:00:16 +0100

    submitted cpp00

--- cpp_00/ex02/src/Account.cpp
+++ cpp_00/ex02/src/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: icunha-t <icunha-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 16:03:17 by icunha-t          #+#    #+#             */
-/*   Updated: 2025/08/08 13:18:34 by icunha-t         ###   ########.fr       */
+/*   Updated: 2025/08/08 13:59:54 by icunha-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,15 @@
 /*                      Variable initiation                        */
 /*******************************************************************/
 
-//initializing static vars outside class
-//vars regarding the whole system
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
 /*******************************************************************/
-/*                    Static public functions                      */
+/*                    Static member functions                      */
 /*******************************************************************/
 
-//implement static functions:
 int Account::getNbAccounts(){
 	return _nbAccounts;
 }
@@ -57,13 +54,13 @@ void Account::displayAccountsInfos(){
 }
 
 /*******************************************************************/
-/*                 Constructor and Destructor                      */
+/*                    Constructor and Destructor                   */
 /*******************************************************************/
 
-Account:: Account(int initial_deposit): 
+Account:: Account(int initial_deposit):
 	_accountIndex(_nbAccounts++),
-	_amount(initial_deposit), 
-	_nbDeposits(0), 
+	_amount(initial_deposit),
+	_nbDeposits(0),
 	_nbWithdrawals(0){
 		_totalAmount += initial_deposit;
 		_displayTimestamp();
@@ -75,27 +72,27 @@ Account:: Account(int initial_deposit):
 
 Account:: ~Account(){
 	_displayTimestamp();
-	std::cout <<" index:" 
-			  << _accountIndex 
+	std::cout <<" index:"
+			  << _accountIndex
 			  << ";amount:" << _amount
 			  << ";closed" << std::endl;
 }
 
 /*******************************************************************/
-/*            Member public functions (per instance)               */
+/*                 Member functions (per instance)                 */
 /*******************************************************************/
 
 void Account::makeDeposit(int deposit){
-	_totalAmount += deposit; //update total amount
-	_totalNbDeposits++; //update total nb of deposits
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex
-			  << ";p_amount:" << _amount //before deposit
+			  << ";p_amount:" << _amount
 			  << ";deposit:" << deposit;
 	_amount += deposit;
 	_nbDeposits++;
-	std::cout <<";amount:" << _amount //after deposit
+	std::cout <<";amount:" << _amount
 			  << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
@@ -135,7 +132,7 @@ void Account::displayStatus() const{
 }
 
 /*******************************************************************/
-/*                       Static prvate function                     */
+/*                     Static private function                     */
 /*******************************************************************/
 
 void Account::_displayTimestamp(){
