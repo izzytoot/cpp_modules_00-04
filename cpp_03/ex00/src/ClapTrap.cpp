bdfb4002dda4433f06d067c372daa68c3cf525a1
isabeltootill <isabeltootill@gmail.com>
2025-08-15 16:59:56 +0100

    ex00 done

--- cpp_03/ex00/src/ClapTrap.cpp
+++ cpp_03/ex00/src/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: isabeltootill <isabeltootill@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 11:43:25 by isabeltooti       #+#    #+#             */
-/*   Updated: 2025/08/15 13:13:11 by isabeltooti      ###   ########.fr       */
+/*   Updated: 2025/08/15 16:55:34 by isabeltooti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ ClapTrap::ClapTrap(const ClapTrap& src){
 
 ClapTrap::~ClapTrap() {
 	if (_name != "to be named")
-		std::cout << BRED << _name << " was destroyed" << RES << std::endl << std::endl;
+		std::cout << BRED << _name << " was destroyed" << RES << std::endl;
 	else
-		std::cout << BRED << "nameless ClapTrap was destroyed" << RES << std::endl << std::endl;
+		std::cout << BRED << "nameless ClapTrap was destroyed" << RES << std::endl;
 }
 
 /******************************************************************************/
@@ -73,66 +73,84 @@ int ClapTrap::getDamage(){
 /******************************************************************************/
 
 void ClapTrap::attack(const std::string& target){
+	if (target.empty()){
+		std::cout << RED << "ERROR: target needed to perform attack." << RES << std::endl;
+		return ;
+	}
+	
 	std::cout << BCYA << this->_name << " is trying to attack " << target << RES << std::endl;
 	
 	if (this->_hit <= 0){
-		std::cout << RED << this->_name << "needs more hit points to attack." << RES << std::endl;
+		std::cout << RED << "ERROR:" << this->_name << " needs more hit points to attack." << RES << std::endl;
 		return ;
 	}
 	if (this->_energy <= 0){
-		std::cout << RED << this->_name << "needs more energy points to attack." << RES << std::endl;
+		std::cout << RED << "ERROR:" << this->_name << " needs more energy points to attack." << RES << std::endl;
 		return ;
 	}
-	
-	std::cout << YEL << this->_name << " attacked " << target << " with xx points of damage." << RES << std::endl;
+
 	this->_energy--;
-	std::cout << YEL << this->_name << " has " << this->_energy << " energy points remaining." << RES << std::endl;
+	
+	std::cout << YEL << this->_name << " attacked " << target << " with " << this->_damage << " points of damage." << RES << std::endl;
+	std::cout << YEL << this->_name << " spent 1 energy point on this attack and now has " << this->_energy << " energy points remaining." << RES << std::endl;
 
 	return ;
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
-	std::cout << BCYA << this->_name << " is trying to take damage of " << amount << " hit points." RES << std::endl;
-
-	int sub = this->_hit - amount;
-	
-	if (sub < 0){
-		std::cout << RED << this->_name << "needs can't take the damage." << RES << std::endl;
-		std::cout << YEL << this->_name << " has now 0 hit points." << RES << std::endl;
-		this->_hit = 0;
+	if ((int)amount < 0){
+		std::cout << RED << "ERROR: invalid amount value." << RES << std::endl;
 		return ;
 	}
 	
-	this->_hit = sub;
-	
-	std::cout << YEL << this->_name << " was attacked with " << amount << " points of damage." << RES << std::endl;
-	std::cout << YEL << this->_name << " has " << this->_hit << " hit points." << RES << std::endl;
+	std::cout << BCYA << this->_name << " is trying to take a damage of " << amount << " hit points." RES << std::endl;
 
-	return ;
+	if (this->_hit == 0){
+		std::cout << RED << "ERROR:" << this->_name << " is already dead." << RES << std::endl;
+		return ;
+	}
+
+	int sub = this->_hit - amount;	
+	
+	if (sub < 0){
+		std::cout << RED << this->_name << " can't take the damage. Needs more hit points." << RES << std::endl;
+		std::cout << RED << this->_name << " has now 0 hit points." << RES << std::endl;
+		this->_hit = 0;
+		return ;
+	}
+	else{
+		this->_hit = sub;
+	
+		std::cout << YEL << this->_name << " was attacked with " << amount << " points of damage." << RES << std::endl;
+		std::cout << YEL << this->_name << " has " << this->_hit << " hit points." << RES << std::endl;
+		
+		return ;
+	}
 }
 
 
 void ClapTrap::beRepaired(unsigned int amount){
-	std::cout << BCYA << this->_name << " is trying to recover " << amount << "hit points." << RES << std::endl;
+	if ((int)amount < 0){
+		std::cout << RED << "ERROR: invalid amount value." << RES << std::endl;
+		return ;
+	}
+	
+	std::cout << BCYA << this->_name << " is trying to recover " << amount << " hit points." << RES << std::endl;
 	
 	if (this->_hit <= 0){
-		std::cout << RED << this->_name << "needs more hit points to be repaired." << RES << std::endl;
+		std::cout << RED << "ERROR: " << this->_name << " needs more hit points to be repaired." << RES << std::endl;
 		return ;
 	}
 	if (this->_energy <= 0){
-		std::cout << RED << this->_name << "needs more energy points to be repaired." << RES << std::endl;
+		std::cout << RED << "ERROR: " << this->_name << " needs more energy points to be repaired." << RES << std::endl;
 		return ;
 	}
 	
-	this->_hit += amount;
-	std::cout << YEL << this->_name << " was repaired with " << amount << " points of damage." << RES << std::endl;
-	std::cout << YEL << this->_name << " has " << this->_hit << " hit points." << RES << std::endl;
-
 	this->_energy--;
-	std::cout << YEL << this->_name << " has " << this->_energy << " energy points remaining." << RES << std::endl;
+	this->_hit += amount;
+	
+	std::cout << YEL << this->_name << " was repaired with " << amount << " hit points." << RES << std::endl;
+	std::cout << YEL << this->_name << " has " << this->_hit << " hit points." << RES << std::endl;
+	std::cout << YEL << this->_name << " spent 1 energy point on this repair and now has " << this->_energy << " energy points remaining." << RES << std::endl;
 	return ;
 }
-
-//energy points should be taken at the beginning or the end of attack and repair?
-//can repair be made on an obj with 0 hit points?
-//can't default constructor have no attributes?
